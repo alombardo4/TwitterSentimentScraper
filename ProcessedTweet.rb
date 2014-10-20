1aca9ff46500b645e48e9565db394040b4a19ab8
Alec Lombardo <alec.lombardo@gmail.com>
2014-10-20 15:22:21 -0400

    Building up processed tweet for requirements

--- ProcessedTweet.rb
+++ ProcessedTweet.rb
@@ -8,11 +8,12 @@ class ProcessedTweet
 				entity_list = entity_list + entity + '|'
 			end
 		end 
-		
-		return [text, hashtags, retweeted, in_reply_to_user_id, user_mentions, retweet_count, created_at, entity_list]
+		is_text = text.length > 0 ? 'y' : 'n'
+		is_question = text.contains('?') ? 'y' : 'n'
+		return [text, hashtags, retweeted, in_reply_to_user_id, user_mentions, retweet_count, created_at, entity_list, text.gsub(/\s+/, "").length, '?', '?', '?', '?', '?', is_text, '?', is_question, '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?']
 	end
 
 	def csv_header
-		return ['text', 'hashtags', 'retweeted', 'in_reply_to_user_id', 'user_mentions', 'retweet_count', 'created_at', 'entities']
+		return ['text', 'hashtags', 'retweeted', 'in_reply_to_user_id', 'user_mentions', 'retweet_count', 'created_at', 'entities','characters', 'picture', 'people', 'product', 'video', 'video-length', 'text', 'humor', 'question', 'invitation/call', 'product info', 'brand info', 'event info', 'monetary benefit', 'functional benefit', 'emotional/psych benefit', 'societal benefit', 'new product announcement', 'new package announcement', 'event context', 'time from event', 'fit with event']
 	end
 end
